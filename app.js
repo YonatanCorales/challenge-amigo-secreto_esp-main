6644d1f2e8774ca2dcaa155998578e693536873e
Yonatan Corales <yonatancoralesb@gmail.com>
2025-02-16 20:05:21 -0500

    función para actualizar la lista de amigos

--- app.js
+++ app.js
@@ -9,8 +9,9 @@ function limpiarCampo() {
 
 //Funcion para agregar amigos a la lista
 function agregarAmigo(){
-    let nombreIngresado = document.getElementById('amigo').value;
-
+    let nombreIngresado = document.getElementById('amigo').value.trim();
+    nombreIngresado = nombreIngresado.toUpperCase()
+    
     //validacion si el campo esta vacio
     if(nombreIngresado === ""){
         alert("Por favor, inserte un nombre.");
@@ -25,5 +26,27 @@ function agregarAmigo(){
     }else{
         amigos.push(nombreIngresado);
     }
+    actualizarLista();
+    
     limpiarCampo();
+
+}
+
+//función para actualizar la lista de amigos
+function actualizarLista(){
+    let lista = document.getElementById('listaAmigos');
+    
+    //Limpiar la lista existente
+    lista.innerHTML = "";
+    
+    //Iterar sobre el arreglo
+    for (let i = 0; i < amigos.length; i++) {
+        let nuevoAmigo = document.createElement("li");
+        nuevoAmigo.textContent = amigos[i];
+        lista.appendChild(nuevoAmigo);
+      }
+}
+//Llamar a la función cuando se cargue la página
+window.onload = function() {
+  actualizarLista();
 }
